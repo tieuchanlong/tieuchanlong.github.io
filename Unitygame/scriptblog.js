50d9e0b63d74fc8f5eb078967273820d70f6f7ed
TieuKhang <52084792+TieuKhang@users.noreply.github.com>
2020-03-30 20:17:30 -0400

    tk

---
+++ Unitygame/scriptblog.js
@@ -0,0 +1,47 @@
+﻿document.getElementById("btntoggle").onclick = function () {
+    if (document.getElementById("topbar").style.marginTop == "-270px") {
+        document.getElementById("topbar").style.marginTop = "0px";
+    } else {
+        document.getElementById("topbar").style.marginTop = "-270px";
+    }
+    if (document.getElementById("content").style.paddingTop == "10px") {
+        document.getElementById("content").style.paddingTop = "280px";
+    } else {
+        document.getElementById("content").style.paddingTop = "10px";
+    }
+}
+
+var text, parser, xmlDoc;
+text = "<index>" +
+    "<lesson>" +
+    "<title>Unity game</title>" +
+    "<url>Unitygame/unitygame.html</url>" +
+    "</lesson>" +
+    "<lesson>" +
+    "<title> C# projects</title>" +
+    "<url>indexblog.html</url>" +
+    "</lesson>" +
+    "<lesson>" +
+    "<title>Augmented Reality project</title>" +
+    "<url>indexblog.html</url>" +
+    "</lesson>" +
+    "<lesson>" +
+    "<title>Contributors</title>" +
+    "<url>indexblog.html</url>" +
+    "</lesson>" +
+    "</index>";
+
+parser = new DOMParser();
+xmlDoc = parser.parseFromString(text, "text/xml");
+var n = xmlDoc.getElementsByTagName("title").length;
+for (var i = 0; i < n; ++i) {
+    var title = xmlDoc.getElementsByTagName("title")[i].childNodes[0].nodeValue;
+    var url = xmlDoc.getElementsByTagName("url")[i].childNodes[0].nodeValue;
+    var ul = document.getElementById("choiceList");
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.setAttribute("href", url);
+    a.appendChild(document.createTextNode(title));
+    li.appendChild(a);
+    ul.appendChild(li);
+}
